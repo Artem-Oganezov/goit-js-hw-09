fddcb558b37a55977bad88e5997b8b034b386a7a
Artem-Oganezov <artem_oga@i.ua>
2025-02-03 10:02:39 +0200

    add new file

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -90,7 +90,7 @@ ulElem.insertAdjacentHTML("beforeend", picturesTemplate(images))
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
 
 
 
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,54 @@
-console.log("Hello World");
+
+const storageKey = "feedback-form-state";
+const formData = {
+    form: document.querySelector('.feedback-form')
+}
+
+formData.form.addEventListener('input', (e) => {
+    const inpEmail = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+    const data = { inpEmail, message };
+    saveToLS(storageKey, data);
+})
+
+function loadLs() {
+  const data = loadFromLS(storageKey);
+  formData.form.elements.email.value = data?.inpEmail || '';
+  formData.form.elements.message.value = data?.message || '';
+}
+
+loadLs();
+
+formData.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inpEmail = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+    const data = { inpEmail, message };
+
+      if (!data.inpEmail || !data.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+
+    console.log(data)
+    localStorage.removeItem(storageKey);
+    e.target.reset();
+}
+)
+
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const body = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(body);
+    return data;
+  } catch{
+    return body;
+  }
+}
